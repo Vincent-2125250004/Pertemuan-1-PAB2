8c8e4244feeefcbb1c544b215454947632ff6c17
Vincent-2125250004 <vincent.cent@mhs.mdp.ac.id>
2023-03-01 09:26:16 +0700

    Pertemuan 4
    Lanjutan Pertemuan 2 Coba Login

--- app/src/main/java/com/if4a/cobalogin/MainActivity.java
+++ app/src/main/java/com/if4a/cobalogin/MainActivity.java
@@ -4,23 +4,39 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
     KendaliLogin KL = new KendaliLogin();
-    private TextView tvSesi;
+    private TextView tvSesi, tvname;
     private String ambilSesi;
     private MaterialButton BtnLogout;
     public static String keySPusername ="spPadang";
+    public static String keySPname ="spPadangname";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (KL.isLogin(MainActivity.this,keySPusername)== true){
             setContentView(R.layout.activity_main);
             tvSesi = findViewById(R.id.tv_sesi);
+            tvname = findViewById(R.id.tv_name);
             BtnLogout = findViewById(R.id.btn_logout);
+
+            tvSesi.setText(KL.getPref(MainActivity.this,keySPusername));
+            tvname.setText(KL.getPref(MainActivity.this,keySPname));
+
+            BtnLogout.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    KL.setPref(MainActivity.this, keySPusername, null);
+                    KL.setPref(MainActivity.this, keySPname, null);
+                    startActivity(new Intent(MainActivity.this, activity_login.class));
+                    finish();
+                }
+            });
         }
         else {
             startActivity(new Intent(MainActivity.this, activity_login.class));
--- app/src/main/java/com/if4a/cobalogin/activity_login.java
+++ app/src/main/java/com/if4a/cobalogin/activity_login.java
@@ -2,13 +2,63 @@ package com.if4a.cobalogin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
 
 public class activity_login extends AppCompatActivity {
+
+    private EditText etUsername, etPassword;
+    private MaterialButton BtnLogin;
+    private String username, password;
+
+    KendaliLogin KL = new KendaliLogin();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        etUsername = findViewById(R.id.et_username);
+        etPassword = findViewById(R.id.et_password);
+        BtnLogin = findViewById(R.id.btn_login);
+
+        BtnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                username = etUsername.getText().toString();
+                password = etPassword.getText().toString();
+
+
+                if (username.trim().isEmpty()){
+                    etUsername.setError("Username Tidak Boleh Kosong");
+                }
+                else if (password.trim().isEmpty()) {
+                    etPassword.setError("Password Tidak Boleh Kosong");
+                }
+                else {
+                    if (username.equals("Cent")&& password.equals("hehe")){
+                        KL.setPref(activity_login.this,MainActivity.keySPusername, username);
+                        KL.setPref(activity_login.this, MainActivity.keySPname, "Vincent");
+                        startActivity(new Intent(activity_login.this, MainActivity.class));
+                        finish();
+                    }
+                    else if (username.equals("Apek")&& password.equals("hehehe")){
+                        KL.setPref(activity_login.this,MainActivity.keySPusername, username);
+                        KL.setPref(activity_login.this, MainActivity.keySPname, "PekCun");
+                        startActivity(new Intent(activity_login.this, MainActivity.class));
+                        finish();
+                    }
+                    else {
+                        Toast.makeText(activity_login.this, "Username atau Password salah", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
     }
 }
